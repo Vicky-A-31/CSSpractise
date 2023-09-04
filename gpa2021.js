77871fd4242382c615319714ab82db8c0ffdc7f4
Vignesh.A <118335617+Vicky-A-31@users.noreply.github.com>
2023-09-04 21:30:23 +0530

    Add files via upload

--- gpa2021.js
+++ gpa2021.js
@@ -2,14 +2,14 @@ function ShowSemester(){
     let dept =  document.getElementById('dept').value
     let semester =  Number(document.getElementById('sem').value)
 
-    let Sem1_2017 = document.getElementById('1stsem2017')
-    let Sem2_2017 = document.getElementById('2ndsem2017')
-    let Sem3_2017 = document.getElementById('3rdsem2017')
-    let Sem4_2017 = document.getElementById('4thsem2017')
-    let Sem5_2017 = document.getElementById('5thsem2017')
-    let Sem6_2017 = document.getElementById('6thsem2017')
-    let Sem7_2017 = document.getElementById('7thsem2017')
-    let Sem8_2017 = document.getElementById('8thsem2017')
+    let Sem1_2021 = document.getElementById('1stsem2021')
+    let Sem2_2021 = document.getElementById('2ndsem2021')
+    let Sem3_2021 = document.getElementById('3rdsem2021')
+    let Sem4_2021 = document.getElementById('4thsem2021')
+    let Sem5_2021 = document.getElementById('5thsem2021')
+    let Sem6_2021 = document.getElementById('6thsem2021')
+    let Sem7_2021 = document.getElementById('7thsem2021')
+    let Sem8_2021 = document.getElementById('8thsem2021')
     let inputSem1 = document.getElementsByClassName('sem1')
     let inputSem2 = document.getElementsByClassName('sem2')
     let inputSem3 = document.getElementsByClassName('sem3')
@@ -21,7 +21,7 @@ function ShowSemester(){
 
     let inputs = document.getElementsByClassName('inputs')
 
-    const hidearray = [Sem1_2017,Sem2_2017,Sem3_2017,Sem4_2017,Sem5_2017,Sem6_2017,Sem7_2017,Sem8_2017]
+    const hidearray = [Sem1_2021,Sem2_2021,Sem3_2021,Sem4_2021,Sem5_2021,Sem6_2021,Sem7_2021,Sem8_2021]
     // initially hide all semesters and buttons, result
     for(let i=0; i<hidearray.length; i++){
         hidearray[i].style.display = 'none'
@@ -34,54 +34,53 @@ function ShowSemester(){
 
     // check the condition which semester to view
     if((dept === "becse") && (semester == 1)){
-        Sem1_2017.style.display = "block"
+        Sem1_2021.style.display = "block"
         for(let i=0; i<inputs.length; i++){
             inputs[i].style.display = "block"
         }
     }
     else if((dept === "becse") && (semester == 2)){
-        Sem2_2017.style.display = "block"
+        Sem2_2021.style.display = "block"
         for(let i=0; i<inputs.length; i++){
             inputs[i].style.display = "block"
         }
     }
     else if((dept === "becse") && (semester == 3)){
-        Sem3_2017.style.display = "block"
+        Sem3_2021.style.display = "block"
         for(let i=0; i<inputs.length; i++){
             inputs[i].style.display = "block"
         }
     }
     else if((dept === "becse") && (semester == 4)){
-        Sem4_2017.style.display = "block"
+        Sem4_2021.style.display = "block"
         for(let i=0; i<inputs.length; i++){
             inputs[i].style.display = "block"
         }
     }
     else if((dept === "becse") && (semester == 5)){
-        Sem5_2017.style.display = "block"
+        Sem5_2021.style.display = "block"
         for(let i=0; i<inputs.length; i++){
             inputs[i].style.display = "block"
         }
     }
     else if((dept === "becse") && (semester == 6)){
-        Sem6_2017.style.display = "block"
+        Sem6_2021.style.display = "block"
         for(let i=0; i<inputs.length; i++){
             inputs[i].style.display = "block"
         }
     }
     else if((dept === "becse") && (semester == 7)){
-        Sem7_2017.style.display = "block"
+        Sem7_2021.style.display = "block"
         for(let i=0; i<inputs.length; i++){
             inputs[i].style.display = "block"
         }
     }
     else if((dept === "becse") && (semester == 8)){
-        Sem8_2017.style.display = "block"
+        Sem8_2021.style.display = "block"
         for(let i=0; i<inputs.length; i++){
             inputs[i].style.display = "block"
         }
     }
-    
     // hide the checked radio buttons
     const radioArray = [inputSem1,inputSem2,inputSem3,inputSem4,inputSem5,inputSem6,inputSem7,inputSem8]
     for(i=0; i<radioArray.length; i++){
@@ -89,7 +88,6 @@ function ShowSemester(){
             radioArray[i][j].checked = false
         }
     }
-    
 }
 
 function CalculateGPA(){
@@ -102,32 +100,32 @@ function CalculateGPA(){
     let inputSem7 = document.getElementsByClassName('sem7')
     let inputSem8 = document.getElementsByClassName('sem8')
 
-    let Sem1_2017 = document.getElementById('1stsem2017')
-    let Sem2_2017 = document.getElementById('2ndsem2017')
-    let Sem3_2017 = document.getElementById('3rdsem2017')
-    let Sem4_2017 = document.getElementById('4thsem2017')
-    let Sem5_2017 = document.getElementById('5thsem2017')
-    let Sem6_2017 = document.getElementById('6thsem2017')
-    let Sem7_2017 = document.getElementById('7thsem2017')
-    let Sem8_2017 = document.getElementById('8thsem2017')
+    let Sem1_2021 = document.getElementById('1stsem2021')
+    let Sem2_2021 = document.getElementById('2ndsem2021')
+    let Sem3_2021 = document.getElementById('3rdsem2021')
+    let Sem4_2021 = document.getElementById('4thsem2021')
+    let Sem5_2021 = document.getElementById('5thsem2021')
+    let Sem6_2021 = document.getElementById('6thsem2021')
+    let Sem7_2021 = document.getElementById('7thsem2021')
+    let Sem8_2021 = document.getElementById('8thsem2021')
     let output = document.getElementsByClassName('inputs')
 
-    let sem1credits = [4,4,3,3,3,4,2,2]
-    let sem1creditstotal = 25;
-    let sem2credits = [4,4,3,3,3,3,2,2]
-    let sem2creditstotal = 24;
-    let sem3credits = [4,4,3,3,3,2,2,2,1]
-    let sem3creditstotal = 24;
-    let sem4credits = [4,3,3,3,3,3,2,2,1]
-    let sem4creditstotal = 24;
-    let sem5credits = [4,3,3,3,3,3,2,2,2]
-    let sem5creditstotal = 25;
-    let sem6credits = [3,3,3,4,3,3,2,2,1,1]
-    let sem6creditstotal = 25;
-    let sem7credits = [3,3,3,3,3,3,2,2]
-    let sem7creditstotal = 22;
-    let sem8credits = [3,3,10]
-    let sem8creditstotal = 16;
+    let sem1credits = [3,4,3,3,3,1,2,2,1]
+    let sem1creditstotal = 22;
+    let sem2credits = [2,4,3,3,4,3,1,2,2,2]
+    let sem2creditstotal = 26;
+    let sem3credits = [4,4,3,3,3,1.5,1.5,2,1]
+    let sem3creditstotal = 23;
+    let sem4credits = [3,4,3,4,3,2,1.5,1.5]
+    let sem4creditstotal = 22;
+    let sem5credits = [4,4,3,3,3,3]
+    let sem5creditstotal = 20;
+    let sem6credits = [4,4,3,3,3,3,3]
+    let sem6creditstotal = 23;
+    let sem7credits = [2,3,3,3,3,2]
+    let sem7creditstotal = 16;
+    let sem8credits = [10]
+    let sem8creditstotal = 10;
  
     // function for validate the given grades
     let validate = (semester_credits,givencredits,semcreditstotal) =>{
@@ -149,7 +147,7 @@ function CalculateGPA(){
         }
     }
 
-    if(Sem1_2017.style.display == 'block'){
+    if(Sem1_2021.style.display == 'block'){
         let semester1_credits = []
         for(let i=0; i<inputSem1.length; i++){
             if(inputSem1[i].type = "radio"){
@@ -161,7 +159,7 @@ function CalculateGPA(){
         // call the validate function
         validate(sem1credits,semester1_credits,sem1creditstotal);
     } 
-    else if(Sem2_2017.style.display == 'block'){
+    else if(Sem2_2021.style.display == 'block'){
         let semester2_credits = []
         for(let i=0; i<inputSem2.length; i++){
             if(inputSem2[i].type = "radio"){
@@ -173,7 +171,7 @@ function CalculateGPA(){
         // call the validate function
         validate(sem2credits,semester2_credits,sem2creditstotal);
     }
-    else if(Sem3_2017.style.display == 'block'){
+    else if(Sem3_2021.style.display == 'block'){
         let semester3_credits = []
         for(let i=0; i<inputSem3.length; i++){
             if(inputSem3[i].type = "radio"){
@@ -185,7 +183,7 @@ function CalculateGPA(){
         // call the validate function
         validate(sem3credits,semester3_credits,sem3creditstotal);
     }
-    else if(Sem4_2017.style.display == 'block'){
+    else if(Sem4_2021.style.display == 'block'){
         let semester4_credits = []
         for(let i=0; i<inputSem4.length; i++){
             if(inputSem4[i].type = "radio"){
@@ -197,7 +195,7 @@ function CalculateGPA(){
         // call the validate function
         validate(sem4credits,semester4_credits,sem4creditstotal);
     }
-    else if(Sem5_2017.style.display == 'block'){
+    else if(Sem5_2021.style.display == 'block'){
         let semester5_credits = []
         for(let i=0; i<inputSem5.length; i++){
             if(inputSem5[i].type = "radio"){
@@ -209,7 +207,7 @@ function CalculateGPA(){
         // call the validate function
         validate(sem5credits,semester5_credits,sem5creditstotal);
     }
-    else if(Sem6_2017.style.display == 'block'){
+    else if(Sem6_2021.style.display == 'block'){
         let semester6_credits = []
         for(let i=0; i<inputSem6.length; i++){
             if(inputSem6[i].type = "radio"){
@@ -221,7 +219,7 @@ function CalculateGPA(){
         // call the validate function
         validate(sem6credits,semester6_credits,sem6creditstotal);
     }
-    else if(Sem7_2017.style.display == 'block'){
+    else if(Sem7_2021.style.display == 'block'){
         let semester7_credits = []
         for(let i=0; i<inputSem7.length; i++){
             if(inputSem7[i].type = "radio"){
@@ -233,7 +231,7 @@ function CalculateGPA(){
         // call the validate function
         validate(sem7credits,semester7_credits,sem7creditstotal);
     } 
-    else if(Sem8_2017.style.display == 'block'){
+    else if(Sem8_2021.style.display == 'block'){
         let semester8_credits = []
         for(let i=0; i<inputSem8.length; i++){
             if(inputSem8[i].type = "radio"){
